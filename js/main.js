7d47a96a0a9d97e76df4b9f4cea6c1ec4585ff57
shadaileng <qpf0510@qq.com>
2019-01-11 15:36:58 +0800

    sidebar

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $(function(){
 		$(item).css({
 			backgroundImage: 'url(' + src + ')'
 		})
+	});
+	$('#dl_navbar_nav a').each((index, item) => {
+		let href = $(item).attr('href')
+		let obj = $(href.substr(0, href.length - 1))
+		if ($(obj).context) {
+			$(item).on('click', () => {
+				$('html, body').animate({scrollTop: $(obj).offset().top}, 500)
+			})
+		}
 	})
+	// $('html body').on('scroll',)
 });
